f3fb0052f9fe7919efcfe44c49f7ba9cf4f5896b
stefKristijan <stef.kristijan@gmail.com>
2017-04-21 13:30:08 +0200

    sad bi trebalo radit

--- tablescript.php
+++ tablescript.php
@@ -12,8 +12,9 @@
  h3 { font-size: 1.2em; }
  table { margin-top: 0.75em; }
  th { font-size: 1.2em; text-align: left; border: none; padding-left: 0;
+ td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
 }
-td { padding: 0.25em 2em 0.25em 0em; border: 0 none; }
+
 </style>
 </head>
 <body>
