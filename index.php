52639a7e450eaaff85f838c723fb9c1dbf5ca28a
stefKristijan <stef.kristijan@gmail.com>
2017-04-21 13:31:14 +0200

    sad bi trebalo radit

--- index.php
+++ index.php
@@ -12,10 +12,5 @@ $conn->setAttribute( PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION );
 catch(Exception $e){
 die(var_dump($e));
 }
-$sql_create = "CREATE TABLE registration_tbl(id INT NOT NULL
-AUTO_INCREMENT, PRIMARY KEY(id), name VARCHAR(30), email
-VARCHAR(30), date DATE);
-";
-$stmt = $conn->prepare($sql_create);
-$stmt->execute();
+
 ?>
