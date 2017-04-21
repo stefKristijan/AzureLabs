cc5a5386a5b04e49f6cf7de5d1d937897b4a97b1
stefKristijan <stef.kristijan@gmail.com>
2017-04-21 13:12:11 +0200

    potpuna konfiguracija

--- tablescript.php
+++ tablescript.php
@@ -1,9 +1,9 @@
 <?php
-$host = "127.0.0.1";
+$host = "127.0.0.1:52972";
 $user = "azure";
 $pwd = "6#vWHD_$";
 $db = "localdb";
-//Database=localdb;Data Source=127.0.0.1;User Id=azure;Password=6#vWHD_$
+//Database=localdb;Data Source=127.0.0.1:52972;User Id=azure;Password=6#vWHD_$
 // Connect to database.
 try {
 $conn = new PDO( "mysql:host=$host;dbname=$db", $user, $pwd);
